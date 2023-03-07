bfeacc034d7147761dd5b3c0e9818797e4d18f80
amirSolidity <amirrezaniyzi70@gmail.com>
2023-03-07 04:03:48 -0800

    searchInput background

---
+++ index.js
@@ -0,0 +1,7 @@
+const searchInput = document.querySelector(".searchInput")
+const searchDiv = document.querySelector(".searchDiv")
+const searchIcon = document.querySelector(".searchIcon")
+
+searchInput.addEventListener('click', () => {
+    searchInput.style.backgroundColor = '#282828'
+})
